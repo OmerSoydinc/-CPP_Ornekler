065b6814490a1691aa2e34e6145abf7b4de46ca8
Ömer <omersoydinc3232@gmail.com>
2023-01-16 00:02:07 +0300

    Add files via upload

---
+++ 26_Define (Makrolar).cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<locale.h>
+#include<conio.h>
+
+
+using namespace std;
+#define getmax(a,b) ((a)>(b) ? (a):(b))
+#define ortalamaal(v1,v2) ((v1+v2)/2)
+
+int main()
+{
+	setlocale(LC_ALL,"Turkish");
+	
+	int x=5,y;
+	y= getmax (x,2);  //x mi büyük 2 mi ---> büyük olaný y ye ata
+	cout<<y<<endl;
+	cout<<getmax(7,x)<<endl;   //7 mi büyük 2xmi ---> büyük olaný alýr ve ekrana yazar
+	
+	//------------------------------------------
+	
+	int v1=10 ,v2=20 ,sonuc;
+	
+	sonuc=ortalamaal(v1,v2);    //   (v1+v2)/2  iþlemini yapar
+	cout<<sonuc<<endl;
+	
+	cout<<ortalamaal(50,6)<<endl;	
+	system("pause");
+	return 0;	
+}
+
