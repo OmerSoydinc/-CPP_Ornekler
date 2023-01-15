bafb243c8a6217affb607784c7888bd85ee66c91
Ömer <omersoydinc3232@gmail.com>
2023-01-16 00:02:54 +0300

    Update 26_Define (Makrolar).cpp

--- 26_Define (Makrolar).cpp
+++ 26_Define (Makrolar).cpp
@@ -12,15 +12,15 @@ int main()
 	setlocale(LC_ALL,"Turkish");
 	
 	int x=5,y;
-	y= getmax (x,2);  //x mi büyük 2 mi ---> büyük olanı y ye ata
+	y= getmax (x,2);  //x mi bÃ¼yÃ¼k 2 mi ---> bÃ¼yÃ¼k olani y ye ata
 	cout<<y<<endl;
-	cout<<getmax(7,x)<<endl;   //7 mi büyük 2xmi ---> büyük olanı alır ve ekrana yazar
+	cout<<getmax(7,x)<<endl;   //7 mi bÃ¼yÃ¼k 2xmi ---> bÃ¼yÃ¼k olani alir ve ekrana yazar
 	
 	//------------------------------------------
 	
 	int v1=10 ,v2=20 ,sonuc;
 	
-	sonuc=ortalamaal(v1,v2);    //   (v1+v2)/2  işlemini yapar
+	sonuc=ortalamaal(v1,v2);    //   (v1+v2)/2  islemini yapar
 	cout<<sonuc<<endl;
 	
 	cout<<ortalamaal(50,6)<<endl;	
