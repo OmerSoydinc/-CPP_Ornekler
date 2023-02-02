1d24d63374a87cbe041ef5b613da586adce2be1f
Ömer <omersoydinc3232@gmail.com>
2023-02-02 20:51:04 +0300

    Add files via upload

---
+++ 40_ Girilen İki Sayidan Büyük Olanı Bulma.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<locale.h>
+#include<conio.h>
+ using namespace std;
+ 
+ int main()
+{  
+  int sayi1,sayi2;
+  
+  cout<<"iki sayi giriniz= ";
+  cin>>sayi1; 
+  cout<<"iki sayi giriniz= ";
+cin>>sayi2;
+  
+  if (sayi1>sayi2)
+     cout<<"Buyuk Olan Sayi= "<<sayi1<<endl;
+     else
+     cout<<"Buyuk Olan Sayi= "<<sayi2<<endl;
+     
+     system("pause");
+     return 0;
+
+ }
