2d392a9c88d6bba42651e95cf52b9eaa83722bf0
Ömer <omersoydinc3232@gmail.com>
2023-02-02 20:52:02 +0300

    Update 34_Çok Boyutlu Dizi Kullanarak Random Sayi Üretmeli Örnek.cpp

--- 34_Çok Boyutlu Dizi Kullanarak Random Sayi Üretmeli Örnek.cpp
+++ 34_Çok Boyutlu Dizi Kullanarak Random Sayi Üretmeli Örnek.cpp
@@ -61,30 +61,30 @@ int main()
 	
 		case 2:	
 		cout<<"2.yılın birinci  çeyreğin ortalama cirosu=  "<<(ciro[1][0]%4901+100+ciro[1][1]%4901+100+ciro[1][2]%4901+100)/3<<endl;
-	    cout<<"2.yılın ikinci   çeyreğin ortalama cirosu=   "<<(ciro[1][3]%4901+100+ciro[1][4]%4901+100+ciro[1][5]%4901+100)/3<<endl;
+	    	cout<<"2.yılın ikinci   çeyreğin ortalama cirosu=   "<<(ciro[1][3]%4901+100+ciro[1][4]%4901+100+ciro[1][5]%4901+100)/3<<endl;
 		cout<<"2.yılın üçüncü   çeyreğin ortalama cirosu=   "<<(ciro[1][6]%4901+100+ciro[1][7]%4901+100+ciro[1][8]%4901+100)/3<<endl;
-	    cout<<"2.yılın dördüncü çeyreğin ortalama cirosu= "<<(ciro[1][9]%4901+100+ciro[1][10]%4901+100+ciro[1][11]%4901+100)/3<<endl;
+	    	cout<<"2.yılın dördüncü çeyreğin ortalama cirosu= "<<(ciro[1][9]%4901+100+ciro[1][10]%4901+100+ciro[1][11]%4901+100)/3<<endl;
 		break;
 			
 		case 3:	
 		cout<<"3.yılın birinci  çeyreğin ortalama cirosu=  "<<(ciro[2][0]%4901+100+ciro[2][1]%4901+100+ciro[2][2]%4901+100)/3<<endl;
-	    cout<<"3.yılın ikinci   çeyreğin ortalama cirosu=   "<<(ciro[2][3]%4901+100+ciro[2][4]%4901+100+ciro[2][5]%4901+100)/3<<endl;
+	    	cout<<"3.yılın ikinci   çeyreğin ortalama cirosu=   "<<(ciro[2][3]%4901+100+ciro[2][4]%4901+100+ciro[2][5]%4901+100)/3<<endl;
 		cout<<"3.yılın üçüncü   çeyreğin ortalama cirosu=   "<<(ciro[2][6]%4901+100+ciro[2][7]%4901+100+ciro[2][8]%4901+100)/3<<endl;
-	    cout<<"3.yılın dördüncü çeyreğin ortalama cirosu= "<<(ciro[2][9]%4901+100+ciro[2][10]%4901+100+ciro[2][11]%4901+100)/3<<endl;
+		cout<<"3.yılın dördüncü çeyreğin ortalama cirosu= "<<(ciro[2][9]%4901+100+ciro[2][10]%4901+100+ciro[2][11]%4901+100)/3<<endl;
 		break;	
 		
 		case 4:	
 		cout<<"4.yılın birinci  çeyreğin ortalama cirosu=  "<<(ciro[3][0]%4901+100+ciro[3][1]%4901+100+ciro[3][2]%4901+100)/3<<endl;
-	    cout<<"4.yılın ikinci   çeyreğin ortalama cirosu=   "<<(ciro[3][3]%4901+100+ciro[3][4]%4901+100+ciro[3][5]%4901+100)/3<<endl;
+	    	cout<<"4.yılın ikinci   çeyreğin ortalama cirosu=   "<<(ciro[3][3]%4901+100+ciro[3][4]%4901+100+ciro[3][5]%4901+100)/3<<endl;
 		cout<<"4.yılın üçüncü   çeyreğin ortalama cirosu=   "<<(ciro[3][6]%4901+100+ciro[3][7]%4901+100+ciro[3][8]%4901+100)/3<<endl;
-	    cout<<"4.yılın dördüncü çeyreğin ortalama cirosu= "<<(ciro[3][9]%4901+100+ciro[3][10]%4901+100+ciro[3][11]%4901+100)/3<<endl;
+	    	cout<<"4.yılın dördüncü çeyreğin ortalama cirosu= "<<(ciro[3][9]%4901+100+ciro[3][10]%4901+100+ciro[3][11]%4901+100)/3<<endl;
 		break;
 		
 		case 5:	
 		cout<<"5.yılın birinci  çeyreğin ortalama cirosu=  "<<(ciro[4][0]%4901+100+ciro[4][1]%4901+100+ciro[4][2]%4901+100)/3<<endl;
-	    cout<<"5.yılın ikinci   çeyreğin ortalama cirosu=   "<<(ciro[4][3]%4901+100+ciro[4][4]%4901+100+ciro[4][5]%4901+100)/3<<endl;
+	    	cout<<"5.yılın ikinci   çeyreğin ortalama cirosu=   "<<(ciro[4][3]%4901+100+ciro[4][4]%4901+100+ciro[4][5]%4901+100)/3<<endl;
 		cout<<"5.yılın üçüncü   çeyreğin ortalama cirosu=   "<<(ciro[4][6]%4901+100+ciro[4][7]%4901+100+ciro[4][8]%4901+100)/3<<endl;
-	    cout<<"5.yılın dördüncü çeyreğin ortalama cirosu= "<<(ciro[4][9]%4901+100+ciro[4][10]%4901+100+ciro[4][11]%4901+100)/3<<endl;
+	    	cout<<"5.yılın dördüncü çeyreğin ortalama cirosu= "<<(ciro[4][9]%4901+100+ciro[4][10]%4901+100+ciro[4][11]%4901+100)/3<<endl;
 		break;
 		default:
 		cout<<"HATALI GİRİŞ YAPTINIZ... "<<endl;
